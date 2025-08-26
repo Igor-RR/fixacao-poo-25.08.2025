cb9912e50cb05fe30ea4fde76823e83015183472
ÍgorRueda <igordriguesrueda15@gmail.com>
2025-08-26 00:52:42 +0000

    a

--- src/main/java/app/Hello.java
+++ src/main/java/app/Hello.java
@@ -1,8 +1,18 @@
 package app;
 
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Olá Mundo!!");
-    }
-}
+    public static void main(String[] args){
+        Genero g = new Genero (1, "Aventura");
 
+        Genero g2 = new Genero(2,"");
+
+        System.out.println(g.getNome());
+        System.out.println(g.getId());
+
+        System.out.println(g2.getNome());
+        System.out.println(g2.getId());
+
+    
+    }
+
+}
